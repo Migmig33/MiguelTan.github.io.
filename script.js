f41bb4bcc8d10cd7d5fcf29613ecdcd6209d8e93
Cold <159975954+Migmig33@users.noreply.github.com>
2025-10-03 21:09:49 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+const texts = [
+    "“Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.” – Albert Einstein ",
+    "“Be the change that you wish to see in the world.” – Mahatma Gandhi",
+    "“Life is never made unbearable by circumstances, but only by lack of meaning and purpose.” – Viktor Frankl",
+    "“Happiness is not something ready-made. It comes from your own actions.” – Dalai Lama",
+    "“How wonderful it is that nobody need wait a single moment before starting to improve the world.”― Anne Frank"
+
+
+];
+let index=0;
+function showtext(){
+    document.getElementById("text").textContent = texts[index]
+};
+
+function next() {
+    index= (index + 1) % texts.length;
+    showtext()
+};
+
+function prev() {
+    index = (index - 1 + texts.length) % texts.length;
+    showtext()
+}
+
+
+
+
+function light(){
+    document.body.classList.toggle("light");
+    document.querySelectorAll("section").forEach( sec => {
+        sec.classList.toggle("light");
+    })
+
+    
+const btn = document.getElementById("light-dark-btn");
+const icon = btn.querySelector("i");
+
+if (document.body.classList.contains("light")) {
+ icon.className = "fa-solid fa-moon";
+} else {
+ icon.className = "fa-solid fa-sun";
+}
+   
+}
+
